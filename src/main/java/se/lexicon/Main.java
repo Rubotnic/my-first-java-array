61ca237af454ad8d10547e56e01d7fd874228ce3
Rubotnic <info@rubotnic.com>
2022-06-07 13:55:28 +0200

    My first JAVA array

---
+++ src/main/java/se/lexicon/Main.java
@@ -0,0 +1,19 @@
+package se.lexicon;
+
+public class Main {
+    public static void main(String[] args) {
+
+/*
+1. Write a program which will store elements in an array of type ‘int’ and print it out.
+Expected output: 11 23 39 etc.
+ */
+
+        int[] numbers = new int[] {11, 23, 39, 51};
+
+        System.out.print(numbers[0] + " ");
+        System.out.print(numbers[1] + " ");
+        System.out.print(numbers[2] + " ");
+        System.out.print(numbers[3] + " ");
+
+    }
+}
