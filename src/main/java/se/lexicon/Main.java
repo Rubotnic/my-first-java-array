c41bd5ebf70416ea40fcb14403a08ef1377e1298
Rubotnic <101702711+Rubotnic@users.noreply.github.com>
2022-08-08 22:19:44 +0200

    Update Main.java

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -6,11 +6,11 @@ public class Main {
 1. Write a program which will store elements in an array of type ‘int’ and print it out.
 Expected output: 11 23 39 etc.
  */
-        int[] numbers = new int[] {11, 23, 39, 51};
+        int[] numbers = new int[] {11, 23, 39};
 
         System.out.print(numbers[0] + " ");
         System.out.print(numbers[1] + " ");
         System.out.print(numbers[2] + " ");
-        System.out.print(numbers[3] + " ");
+        System.out.print("etc.");
     }
 }
